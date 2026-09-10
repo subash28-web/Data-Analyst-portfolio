25a8cefe773360e2e9f5c46fb064c1fe9ed70771
Subash Chandra Bose <mcsubash1@gmail.com>
2026-09-10 17:23:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// Smooth scrolling for navigation links
+
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+
+    link.addEventListener("click", function (event) {
+
+        event.preventDefault();
+
+        const target = document.querySelector(this.getAttribute("href"));
+
+        if (target) {
+            target.scrollIntoView({
+                behavior: "smooth"
+            });
+        }
+
+    });
+
+});
